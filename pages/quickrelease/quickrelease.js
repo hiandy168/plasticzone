1080c7072e16295b982a83ab1040100c96b811f5
bbson911 <bbson911@qq.com>
2017-04-28 18:26:39 +0800

    modify

---
+++ pages/quickrelease/quickrelease.js
@@ -0,0 +1,68 @@
+//mysupply.js
+
+//获取应用实例
+var app = getApp();
+Page({
+  data: {
+		name: [],
+		page: 1,
+		condition: true,
+		id: "",
+		user_id: ""
+  },
+  onLoad: function (options) {
+    // 生命周期函数--监听页面加载
+    var _this = this;
+    wx.request({
+      url: app.globalData.apiHost+'/getMyComment',
+      data: {
+			    page: _this.data.page,
+          token: wx.getStorageSync('token'),
+          size: 10
+      },
+      method: 'POST', 
+      header: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      success: function(res){
+        if (res.data.err == 0) {
+          _this.setData({
+            name: res.data.data
+          });
+        }
+       
+      },
+      fail: function() {
+        
+      },
+      complete: function() {
+        
+      }
+    })
+
+  },
+  onReady: function () {
+    // 生命周期函数--监听页面初次渲染完成
+
+  },
+  onShow: function () {
+    // 生命周期函数--监听页面显示
+
+  },
+  onHide: function () {
+    // 生命周期函数--监听页面隐藏
+
+  },
+  onUnload: function () {
+    // 生命周期函数--监听页面卸载
+
+  },
+  onPullDownRefresh: function () {
+    // 页面相关事件处理函数--监听用户下拉动作
+
+  },
+  onReachBottom: function () {
+    // 页面上拉触底事件的处理函数
+
+  }
+})
